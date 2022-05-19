b0bf18b356de2484c4454257aafdc6130928b267
20prapti <105740256+20prapti@users.noreply.github.com>
2022-05-19 22:58:51 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,310 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRIME REPORTING SYSTEM</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo">
+        <img src="images/logo.jpeg" alt="">
+    </a>
+
+    <nav class="navbar">
+        <a href="#home">HOME</a>
+        <a href="#about">ABOUT</a>
+        <a href="userlogin.php" target="_blank">USER LOGIN</a>
+        <a href="official_login.php" target="_blank">OFFICIAL LOGIN</a>
+        <a href="#review">REVIEWS</a>
+        <a href="#contact">CONTACT US</a>
+        <a href="#blogs">BLOGS</a>
+    </nav>
+
+    <div class="icons">
+        <div class="fas fa-search" id="search-btn"></div>
+        
+        
+        <div class="fas fa-bars" id="menu-btn"></div>
+    </div>
+
+    <div class="search-form">
+        <input type="search" id="search-box" placeholder="search here...">
+        <label for="search-box" class="fas fa-search"></label>
+    </div>
+
+   
+        <a href="https://www.instagram.com/kenclub_2022/" target="_blank" class="btn">Ken Club</a>
+    </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>let's cut the crime knife</h3>
+        <p>REGISTER NOW</p>
+        <a href="registration.php"  target="_blank" class="btn">SIGN UP</a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="images/CRS.gif" alt="">
+        </div>
+
+        <div class="content">
+            <h3>what makes our PORTAL special?</h3>
+            <p>This crime reporting portal is an initiative of Governmnet of India to facilitate victims/complaints 
+                to report complaints online. This portal caters to complaints pertaining to any field against the citizens of the nations.
+            </p>
+            <p>Complaints reported on this portal are dealt by law enforcement agencies/ police based on the information
+                available in the complaints. It is imperative to provide correct and accurate details while filling complaint for prompt action.
+            </p>
+            
+            <a href="about.html" target="_blank" class="btn">EXPLORE about TEAM MEMBERS</a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+
+
+
+
+
+<!-- review section starts  -->
+
+<section class="review" id="review">
+
+    <h1 class="heading"> customer's <span>review</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+            <p>"A complaint form bihar may lead to an offender in Punjab . Instaed of sending a team 
+                from here, we connect with the nodal officer in Punjab with ethe help this crime reporting 
+                portal."</p>
+            <img src="images/pic-1.png" class="user" alt="">
+            <h3>charles john</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+            <p>"This portal makes it easier for people to file a complaint since
+                they cannot physically go to the station . i even urge the citizen to use this reporting portal to register 
+                complaint against any fields."
+            </p>
+            <img src="images/pic-2.png" class="user" alt="">
+            <h3>ananya sharma</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+        
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+            <p>
+                "this plateform helps them to gain a perspective and wide overview of what kind of crime are taking place
+                . pursuant to this, an effective and strog legislative framework can be framed so that the crimes can be reduced
+                in the comming years."
+            </p>
+            <img src="images/pic-3.png" class="user" alt="">
+            <h3>rohan singh</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- review section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <h1 class="heading"> <span>contact</span> us </h1>
+
+    <div class="row">
+        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54786.07501106367!2d75.8250016191961!3d30.88303283560982!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a830bfd62a5b7%3A0x56dadbaf3cd7db4e!2sOffice%20ACP%20Central%20Police%20Station%20Division%20No-2%20Ludhiana!5e0!3m2!1sen!2sin!4v1652603745363!5m2!1sen!2sin"  allowfullscreen="" loading="lazy" ></iframe>
+
+
+        <form action="">
+            <h3>Hello!! let's complaint</h3>
+            <div class="inputBox">
+                <span class="fas fa-user"></span>
+                <input type="text" placeholder="name">
+            </div>
+            <div class="inputBox">
+                <span class="fas fa-envelope"></span>
+                <input type="email" placeholder="email">
+            </div>
+            <div class="inputBox">
+                <span class="fas fa-phone"></span>
+                <input type="number" placeholder="number">
+            </div>
+            <input type="submit" value="contact now" class="btn">
+        </form>
+
+    </div>
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- blogs section starts  -->
+
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> our <span>blogs</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/c4 (1).jpeg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">SSP GURPREET SINGH BULLAR</a>
+                <span>JALANDHAR/ 06th may, 2022</span>
+                <p>
+                    A major benefit of online reporting especially during the pandemic , is that it prevents the need for physically
+                    contact during the non- emergency situation.
+                </p>
+                <a href="https://twitter.com/gurpreetcpjal?lang=en" target="_blank" class="btn">read more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/c1.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">IPS officer RAVJAOT KAUR GREWAL</a>
+                <span>PUNJAB/ 24th april, 2022</span>
+                <p>This portal reduces the need of citizens and employees to go into a police department
+                    and wait for a clerk to pull the report . Employees and individuals can download a copy of an incident 
+                    report  for insurance purposes at their own convinience.
+                </p>
+                <a href="https://twitter.com/ravjotgrewalips" target="_blank" class="btn">read more</a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="images/c4 (2).jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">IPS officer KANWARDEEP KAUR</a>
+                <span>LUDHIANA / 1st may, 2022</span>
+                <p>
+                    Allow patrol officers to focus on more urgent , high priority calls where their immediate 
+                    physical presence is needed. enabling online reporting for citizens helps make efficient use of 
+                    limited resources and just one step in the ongoing evolution of service to their communities.
+                </p>
+                <a href="https://twitter.com/kanwarips?lang=en" target="_blank" class="btn">read more</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="share">
+        <a href="https://www.facebook.com/The-Punjab-Police-134307897020614" target="_blank"  class="fab fa-facebook-f"></a>
+        <a href="https://twitter.com/PunjabPoliceInd" target="_blank"   class="fab fa-twitter"></a>
+        <a href="https://www.instagram.com/punjabpoliceind/?hl=en" target="_blank" class="fab fa-instagram"></a>
+        <a href="https://www.linkedin.com/company/punjabpolice/" target="_blank" class="fab fa-linkedin"></a>
+        <a href="https://in.pinterest.com/search/pins/?q=punjab%20police&rs=typed&term_meta%5B%5D=police%7Ctyped" target="_blank" class="fab fa-pinterest"></a>
+    </div>
+
+    <div class="links">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="registration.php">sign up</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact us</a>
+        <a href="#blogs">blogs</a>
+    </div>
+
+    <div class="credit">created by <span>prapti gupta</span> | all rights reserved</div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
